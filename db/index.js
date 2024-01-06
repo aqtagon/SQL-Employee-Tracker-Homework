f674960c40a369d95d981fa64fdb9e92a8401b30
aqtagon <ahmed.danawi@gmail.com>
2024-01-06 14:41:00 -0800

    Added departments, roles, and employees commands to index.js

--- db/index.js
+++ db/index.js
@@ -7,4 +7,19 @@ class db {
       findAllDepartments() {
         return this.connection.promise().query("SELECT * FROM department");
       }
+      findAllRoles() {
+        return this.connection
+          .promise()
+          .query(
+            "SELECT roles.id, roles.title, roles.salary, department.name AS department FROM roles LEFT JOIN department ON roles.department_id = department.id"
+          );
+      }
+      findAllEmployees() {
+        return this.connection.promise()
+        .query(`SELECT
+        employee.id, CONCAT(employee.first_name, ' ' , employee.last_name) AS name, roles.title, department.name AS department, roles.salary, CONCAT(manager.first_name, ' ' , manager.last_name) AS manager
+        FROM
+        employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id`);
+    }
+    
 }
